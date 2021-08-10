dcb02d4ba3ef0bf2fe4f2ad3e9758416b1b4a016
Krystiankr <56454269+Krystiankr@users.noreply.github.com>
2021-08-10 23:07:39 +0200

    Update 2.2.14_poprawa.py

--- 2.2.14_poprawa.py
+++ 2.2.14_poprawa.py
@@ -1,10 +1,4 @@
 import numpy as np
-n = 51
-ones = np.ones(n).astype('int')
-ones = list(ones)
-ones.count(1)
-squares = [x**2 for x in range(51) if x**2 < 51 and x**2>1]
-main = [ones]
 
 def foo(lista, main, squares):
     for el in squares:
@@ -12,21 +6,27 @@ def foo(lista, main, squares):
         if tmp.count(1) < el:
             continue
         if tmp.count(1) >= 4:
-            podmianka(tmp, el, main)
+            replace(tmp, el, main)
             foo(tmp, main,squares)
-            
-def podmianka(lista, el, main):
+           
+        
+def replace(lista, el, main):
     if lista.count(1) >= el:
         lista.append(el)
         for _ in range(el):
             lista.remove(1)
-        #print(lista)
         lista.sort()
         main.append(lista)
+
+n = 34
+ones = np.ones(n).astype('int')
+ones = list(ones)
+ones.count(1)
+squares = [x**2 for x in range(n) if x**2 < n and x**2>1]
+main = [ones]
         
 foo(ones, main,squares)
 
 main_set = set(tuple(el) for el in main)
-len(main_set)
 for i,el in enumerate(sorted(main_set),start=1):
     print(f'{i}:{el}')
